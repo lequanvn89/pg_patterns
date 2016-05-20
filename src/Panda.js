9adcb1e2d0ffa3b1e10178be2c24508beeed9867
lequanvn89 <lequan.vn89@gmail.com>
2016-05-20 19:24:22 +0300

    Added `Panda` class for testing modules in `ES6`.

---
+++ src/Panda.js
@@ -0,0 +1,13 @@
+class Panda {
+    constructor(name) {
+        this.name = name;
+    }
+    eat() {
+        console.log(`Panda ${this.name} is eating`);
+    }
+    sleep() {
+        console.log(`Panda ${this.name} is sleeping`);
+    }
+}
+
+export default Panda;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Panda from './Panda.js';
+
+let pandaJohn = new Panda('John');
+let pandaPoo = new Panda('Poo');
+pandaJohn.eat();
+pandaJohn.sleep();
+pandaPoo.sleep();
